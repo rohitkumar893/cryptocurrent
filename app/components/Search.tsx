91dc2f3d5baf5479c6ed39f2b055477f129eb523
Rohit <rkrohitkumar893@gmail.com>
2025-10-27 15:45:00 +0530

    remove setSearch

--- app/components/Search.tsx
+++ app/components/Search.tsx
@@ -3,7 +3,7 @@
 export default function Search(){
     return(
     <div>
-        <input className="w-90 h-14 border-2 px-6 border-gray-200 my-12 rounded-xl" placeholder="Search for currency" onChange={(e)=>{setSearch(e.target.value)}} />
+        <input className="w-90 h-14 border-2 px-6 border-gray-200 my-12 rounded-xl" placeholder="Search for currency" />
     </div>
     )
 }
