082335f120c2b62fab92e359dc2fed535ab8770a
Rohit <rkrohitkumar893@gmail.com>
2025-10-29 16:28:48 +0530

    minor fixes

--- app/components/Loading.tsx
+++ app/components/Loading.tsx
@@ -2,10 +2,10 @@
 
 import { DotLottieReact } from "@lottiefiles/dotlottie-react";
 
-export default function LottieAnimation() {
+export default function Loading() {
   return (
     <div className="flex justify-center items-center h-screen w-screen">
-      <div className="h-100">
+      <div className="h-80">
         <DotLottieReact
             src="https://lottie.host/59b5388d-8550-4c73-a114-d2ad99b6715c/6shhQu4ZUF.lottie"
             loop
